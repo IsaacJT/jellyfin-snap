566f1ef21135c094136edf516d71ad6b9cf96ab8
Isaac True <isaac@is.having.coffee>
2024-03-12 18:49:52 +0100

    Add script for starting Jellyfin
    Signed-off-by: Isaac True <isaac@is.having.coffee>

---
+++ scripts/jellyfin.sh
@@ -0,0 +1,28 @@
+#!/bin/sh -ex
+
+REQUIRED_INTERFACES="network network-bind firewall-control"
+OPTIONAL_INTERFACES="home removable-media mount-observe opengl"
+
+for intf in ${REQUIRED_INTERFACES}; do
+        if ! snapctl is-connected "${intf}"; then
+                echo "Required interface is not connected: ${intf}"
+                echo ""
+                echo "Please connect this interface using the following command:"
+                echo "snap connect ${SNAP_NAME}:${intf}"
+                exit 1
+        fi
+done
+
+for intf in ${OPTIONAL_INTERFACES}; do
+        if ! snapctl is-connected "${intf}"; then
+                echo "Optional interface is not connected: ${intf}"
+                echo "This is recommended to ensure best usage of this program."
+                echo ""
+                echo "Please connect this interface using the following command:"
+                echo "snap connect ${SNAP_NAME}:${intf}"
+        fi
+done
+
+exec "${SNAP}"/usr/lib/jellyfin/bin/jellyfin \
+        --ffmpeg "${SNAP}"/usr/lib/jellyfin-ffmpeg/ffmpeg \
+        --restartpath "${SNAP}"/usr/lib/jellyfin/restart.sh
